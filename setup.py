1714b0899fb66476d2cb67d5cae29e7b1d999400
bglid <benjamin.h.glidden@gmail.com>
2024-12-18 13:13:20 -0500

    Adding Setup tools

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Interview_Data_Extractor",
+    version="0.1.0",
+    author="IU",
+    description="Tools for text data extraction + analysis in French",
+    packages=find_packages(),
+)
